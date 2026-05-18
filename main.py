988f6b0c3f8c41b7c3f8c1a75cda528f75d67410
kinghwanjjang1008-hash <kinghwanjjang1008@gmail.com>
2026-05-18 10:09:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ a=st.text_input('왓츠 유얼 네임?')
 b=st.selectbox('좋아하는 사람을 초이스하세요!', ['김진성','양예성','윤희성'])
 if st.button('인삿말 생성'):
   st.write(a+'님, 헬로우~!')
+  st.info('웰컴~')
+  st.warning(b+'이 친구를 좋아하시나봐요~ㅎ.ㅎ')
+  st.error('잘 부탁합니듀')
+
